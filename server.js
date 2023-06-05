cc6a8a12bba5dd6af36a6a03f6eb4e33f91c60b3
Brenden <brenthomas02@gmail.com>
2023-06-05 15:05:28 -0400

    added in app.get in server.js

--- server.js
+++ server.js
@@ -1,10 +1,60 @@
-const express = require('express')
+const fs = require("fs");
 
-const app = express(); 
+const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 3001; 
+const app = express();
+const PORT = 3001;
 
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json') )
+})
+
+// app.delete('/api/notes', (req, res) => {
+//   const {title, text} = req.body; 
+
+//   fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//     if(err) {
+//       console.log(err)
+//     } else {
+//       const parsedData = JSON.parse(data); 
+//       if (parsedData.includes(title, text)) {
+//         console.log('ready to filter out of db')
+//       }
+//     }
+//   })
+// })
+
+app.post("/api/notes", (req, res) => {
+
+    const { title, text } = req.body;
+
+    const newData = {
+        title,
+        text
+      };
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(newData);
+
+      fs.writeFile("db/db.json", JSON.stringify(parsedData, null, 5), (err) => {
+        err ? console.log(err) : console.log("sucess");
+      });
+    }
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`serverlisteningport:${PORT}`)
-})
+  console.log(`serverlisteningport:${PORT}`);
+});
